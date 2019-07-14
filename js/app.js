20215a848ed025732d8ee4d01d9bf0c2032134fd
Daniel <danrerjsa001@gmail.com>
2019-07-14 09:29:32 +0100

    nysc-template

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+	
+
+	var log = document.querySelector('#loginPara');
+	log.addEventListener('mouseover', function(){
+		 this.style.textDecoration='underline'
+	});
+	var log = document.querySelector('#loginPara');
+	log.addEventListener('mouseout', function(){
+		 this.style.textDecoration='none'
+	})
+
+	var img2 = document.querySelector('#img2');
+	img2.addEventListener('mouseover', function(){
+		 this.style.cursor='pointer'
+	})
+
+	var container3 = document.querySelector('.container3');
+	container3.addEventListener('mouseenter', function(){
+		 this.style.backgroundColor='gray'
+	})
+	var container3 = document.querySelector('.container3');
+	container3.addEventListener('mouseleave', function(){
+		 this.style.backgroundColor='#ff7000'
+	})
+	
+	
+
+	
+
+
+
+
